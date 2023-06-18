684871ba8321fe4d9d1254479a6bf9a24da9b6ee
Uwe Hahne <uwe.hahne@hs-furtwangen.de>
2023-06-18 21:38:52 +0200

    Updated database example and exercise

--- database/db_sandbox.js
+++ database/db_sandbox.js
@@ -12,11 +12,29 @@ async function main() {
   });
 
   const tables = await db.all('SELECT name FROM sqlite_schema WHERE type="table" ORDER BY name');
-  console.log(tables);
+  // console.log("Tables: ");
+  // console.log(tables);
 
   // list all students
-    const students = await db.all('SELECT * FROM student');
-    console.log(students);
+  const students = await db.all('SELECT * FROM student');
+  console.log("Students: ");
+  console.log(students);
+  
+  // test inner join
+  const selection = await db.all('SELECT * FROM student INNER JOIN course ON student.course = course.id');
+  // console.log(selection);
+
+  // test another join
+  const selection2 = await db.all('SELECT student.studentNr, student.firstName, student.lastName, course.name AS "course", faculty.name AS "faculty" FROM (student INNER JOIN course ON student.course = course.id) INNER JOIN faculty ON course.faculty = faculty.id');
+  // console.log(selection2);
+
+  // create a view
+  //const view = await db.all('CREATE VIEW student_data AS SELECT student.studentNr, student.firstName, student.lastName, course.name AS "course", faculty.name AS "faculty" FROM (student INNER JOIN course ON student.course = course.id) INNER JOIN faculty ON course.faculty = faculty.id');
+  //console.log(view);
+
+  // show the view
+  //const showView = await db.all('SELECT * FROM student_data');
+  //console.log(showView);
 
   // TODO: list all courses
   
