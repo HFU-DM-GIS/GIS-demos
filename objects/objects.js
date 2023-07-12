7403886daa612e6e285d623d879f0a9c94cc3d2f
Uwe Hahne <uwe.hahne@hs-furtwangen.de>
2023-07-12 17:29:42 +0200

    added examples for object access

--- objects/objects.js
+++ objects/objects.js
@@ -1,19 +1,28 @@
 const p = { name: 'Anna', age: 23 };
-console.log(p);
+// console.log(p);
 
-const people = [
+let people = [
   p,
   { name: 'Oliver', age: 22 },
   { name: 'Peter', age: 21 },
 ];
 
 // print the names of all people
-for (let m of people) {
-  console.log(m.name);
-}
+// for (let m of people) {
+//   console.log(m.name);
+// }
 
 // add another person
+let p2 = { name: 'Horst', age: 31 };
+let more_people = people.concat(p2);
+
+// console.log(people);
+// console.log(more_people);
 
 // stringify with JSON
+let peopleAsString = JSON.stringify(people)
+console.log(peopleAsString);
 
 // parse string to JSON
+let peopleASJSONagain = JSON.parse(peopleAsString);
+console.log(peopleASJSONagain);
